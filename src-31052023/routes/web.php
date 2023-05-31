ee76c82df0477ab8be6d40ffb49091664de44413
Viapak-Singh <vip8work@gmail.com>
2023-05-31 17:40:26 +0530

    delete backup

--- src-31052023/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Vip\Crud\Http\Controllers\CrudController;
-
-Route::group(['middleware' => 'web'], function () {
-    Route::get('vip/index', [CrudController::class, 'index'])->name('index');
-    Route::get('vip/create', [CrudController::class, 'create'])->name('create');
-    Route::post('vip/create', [CrudController::class, 'store'])->name('store');
-});
