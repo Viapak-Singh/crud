6c0146eac18d98e6234dcca5eb5665a200232a24
Viapak-Singh <vip8work@gmail.com>
2023-06-01 09:57:12 +0530

    Update CrudServiceProvider.php

--- src/CrudServiceProvider.php
+++ src/CrudServiceProvider.php
@@ -10,9 +10,9 @@ class CrudServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
-        $this->loadViewsFrom(__DIR__.'/views', 'crud');
-        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
+        // $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+        // $this->loadViewsFrom(__DIR__.'/views', 'crud');
+        // $this->loadMigrationsFrom(__DIR__.'/database/migrations');
 
         // Custom Methods
         $this->loadAssets();
